35f6cd7c7952e2415b813ea1a24f22d436a9c556
Blanc08 <bagusoktaviadi@gmail.com>
2022-04-14 12:50:59 +0700

    result

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  bubbleSort(@Query() query): string {
-    const arr = query.arr.split(',').map(Number);
+  @Post()
+  bubbleSort(@Request() request): string {
+    const arr = request.body.arr.split(',').map(Number);
     return this.appService.bubbleSort(arr);
   }
 }
